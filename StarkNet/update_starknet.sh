85d8e437a6402fc65f22f951faf724289a3cb769
ruslan <71116615+kerakelv@users.noreply.github.com>
2022-09-22 18:44:17 +0300

    Update update_starknet.sh

--- StarkNet/update_starknet.sh
+++ StarkNet/update_starknet.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/kerakelv/Tools/main/kerakelv.sh | bash
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
 echo "-----------------------------------------------------------------------------"
-echo "Починаємо оновлення репрозиторію "
+echo "Начинаем обновление репрозитория "
 echo "-----------------------------------------------------------------------------"
 cd $HOME/pathfinder/py
 git fetch
-git checkout v0.3.4
-echo "Репозиторій успішно оновлено, починаємо білд"
+git checkout v0.3.5
+echo "Репозиторий успешно обновлен, начинаем билд"
 echo "-----------------------------------------------------------------------------"
 rm -rf $HOME/pathfinder/py/.venv
 python3 -m venv .venv
@@ -20,8 +20,35 @@ rustup update
 cargo build --release --bin pathfinder
 sleep 2
 source $HOME/.bash_profile &>/dev/null
-echo "Білд завершено успішно"
+echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
 systemctl restart starknet
-echo "Нода оновлена та запущена"
+echo "Нода обновлена и запущена"
+echo "-----------------------------------------------------------------------------"
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+echo "Начинаем обновление репрозитория "
+echo "-----------------------------------------------------------------------------"
+cd $HOME/pathfinder/py
+git fetch
+git checkout v0.3.5
+echo "Репозиторий успешно обновлен, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+rm -rf $HOME/pathfinder/py/.venv
+python3 -m venv .venv
+source .venv/bin/activate
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+rustup default stable
+rustup update
+cargo build --release --bin pathfinder
+sleep 2
+source $HOME/.bash_profile &>/dev/null
+echo "Билд завершен успешно"
+echo "-----------------------------------------------------------------------------"
+systemctl restart starknet
+echo "Нода обновлена и запущена"
 echo "-----------------------------------------------------------------------------"
