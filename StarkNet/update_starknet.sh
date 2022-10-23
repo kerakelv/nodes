081124565ca58cdcee56ef0fb4ae026c76e53af1
ruslan <71116615+kerakelv@users.noreply.github.com>
2022-10-23 10:09:30 +0300

    v0.3.7

--- StarkNet/update_starknet.sh
+++ StarkNet/update_starknet.sh
@@ -7,7 +7,7 @@ echo "Начинаем обновление репрозитория "
 echo "-----------------------------------------------------------------------------"
 cd $HOME/pathfinder/py
 git fetch
-git checkout v0.3.5
+git checkout v0.3.7
 echo "Репозиторий успешно обновлен, начинаем билд"
 echo "-----------------------------------------------------------------------------"
 rm -rf $HOME/pathfinder/py/.venv
@@ -22,33 +22,6 @@ sleep 2
 source $HOME/.bash_profile &>/dev/null
 echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
-systemctl restart starknet
-echo "Нода обновлена и запущена"
-echo "-----------------------------------------------------------------------------"
-#!/bin/bash
-
-echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
-echo "-----------------------------------------------------------------------------"
-echo "Начинаем обновление репрозитория "
-echo "-----------------------------------------------------------------------------"
-cd $HOME/pathfinder/py
-git fetch
-git checkout v0.3.5
-echo "Репозиторий успешно обновлен, начинаем билд"
-echo "-----------------------------------------------------------------------------"
-rm -rf $HOME/pathfinder/py/.venv
-python3 -m venv .venv
-source .venv/bin/activate
-PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
-PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
-rustup default stable
-rustup update
-cargo build --release --bin pathfinder
-sleep 2
-source $HOME/.bash_profile &>/dev/null
-echo "Билд завершен успешно"
-echo "-----------------------------------------------------------------------------"
-systemctl restart starknet
+sudo systemctl restart starknet
 echo "Нода обновлена и запущена"
 echo "-----------------------------------------------------------------------------"
